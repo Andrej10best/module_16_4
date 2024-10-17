43f0c9fe7805d3a81a1dfb4aa2b99990b785f577
Andrej10best <Andrej10best@yandex.ru>
2024-10-17 09:39:45 +0400

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+users = []
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int = None
+
+
+
+@app.get('/users')
+async def get_users():
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def post_user(username: str, age: int):
+    user_id = len(users) + 1 if users else 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
+
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: int, username: str, age: int):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: int):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail='User was not found')
